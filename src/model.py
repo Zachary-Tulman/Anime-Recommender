b26da6fc6ba6cc02925fb861c5802f288a2527cd
unknown <zrtulman@gmail.com>
2026-05-27 13:50:16 -0700

    created AnimeRecommender model code

--- src/model.py
+++ src/model.py
@@ -1,3 +1,15 @@
 import torch
 import torch.nn as nn
 
+class AnimeRecommender(nn.Module):
+    def __init__(self, num_users: int, embedding_dim_users: int, num_anime: int, embedding_dim_anime: int):
+        super().__init__()
+
+        self.user_embedding = nn.Embedding(num_users, embedding_dim_users)
+        self.anime_embedding = nn.Embedding(num_anime, embedding_dim_anime)
+
+    def forward(self, user_ids, anime_ids):
+        user_embed = self.user_embedding(user_ids)
+        anime_embed = self.anime_embedding(anime_ids)
+
+        prediction = (user_embed * anime_embed).sum(dim=1)
