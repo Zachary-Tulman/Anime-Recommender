0c98aa60358f819cd31eb5578ee20d492c58e701
unknown <zrtulman@gmail.com>
2026-06-04 19:02:49 -0700

    Enriched basic cosine similarity with PMI, split genre/type handling

--- src/content.py
+++ src/content.py
@@ -1,16 +1,32 @@
 import pandas as pd
+import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
 DATA_DIR = "./data/"
 
 data = pd.read_csv(f"{DATA_DIR}anime.csv", sep="\t", usecols=["anime_id", "title", "genres", "type"])
 
-genre_encode = data["genres"].str.get_dummies(sep="|")
+# multi-hot encodings
+genres_encode = data["genres"].str.get_dummies(sep="|")
 type_encode = pd.get_dummies(data["type"])
 
-features = pd.concat([genre_encode, type_encode], axis=1)
+# Pointwise mutual information (PMI)
+genre_counts = genres_encode.sum()
+genre_prob = genre_counts / len(genres_encode)
 
-def get_similarity(query: pd.DataFrame, features: pd.DataFrame, n: int = 10):
-    similarity = cosine_similarity(query, features).flatten()
-    similarity = pd.Series(similarity, index=data["anime_id"]).sort_values(ascending=False).head(n)
+co_occurrence_counts = genres_encode.T.dot(genres_encode)
+joint_probs = co_occurrence_counts / len(genres_encode)
+
+base_probs = np.outer(genre_prob, genre_prob)
+
+pmi = np.maximum(np.log(joint_probs / base_probs), 0)
+
+def get_similarity(genre_query: pd.DataFrame, type_query: pd.DataFrame, n: int = 10, genre_weight: float = 0.775, type_weight: float = 0.225):
+    enriched_genre_query = genre_query.dot(pmi)
+    genre_similarity = cosine_similarity(enriched_genre_query, genres_encode).flatten() * genre_weight
+
+    type_similarity = cosine_similarity(type_query, type_encode).flatten() * type_weight
+
+    similarity = pd.Series(genre_similarity + type_similarity, index=data["title"]).sort_values(ascending=False).head(n)
+    
     return similarity
